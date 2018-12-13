95ce025837335ccd005af1d5b7b5354d38b02576
Kraengsak <invisibilitydada@gmail.com>
2018-12-13 11:36:53 +0700

    edit123

--- testgo.go
+++ testgo.go
@@ -1,11 +1,13 @@
 package main
 
-func main() {  
-	x := 0  
-	increment := func() int {        
-		x++        
-		return x    
-		}    
-		println(increment())    
-		   
+func main() {
+	for i := 1; i <= 100; i++ {	
+		if i % 3 == 0 && i % 5 ==0 {
+			println("FizzBuzz")
+		} else if i % 3 == 0  {
+			println("Fizz")
+		} else if i % 5 == 0 {
+			println("Buzz")
+		}
+	}
 }
